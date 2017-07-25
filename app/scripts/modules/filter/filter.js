3613f28586cd1db68af6593543243b75fc654a6a
Jaume Capdevila Pérez <contact@jaumecapdevila.net>
2017-07-25 22:24:40 +0200

    Improved the algorithm of the filter

--- app/scripts/modules/filter/filter.js
+++ app/scripts/modules/filter/filter.js
@@ -3,21 +3,69 @@ const Filter = function(serializer) {
 }
 Filter.filter = function(firstList, secondList) {
   const response = {};
-  const repeatedItems = [];
-  const itemsToConcat = [];
-  for (let i = 0; i < firstList.length; i++) {
-    for (let k = 0; k < secondList.length; k++) {
-      if (firstList[i] === secondList[k]) {
-        repeatedItems.push(secondList[k]);
+
+  const repeatedItemsInFirsList = [];
+  const filteredFirstList = [];
+  const repeatedItemsInSecondList = [];
+  const filteredSecondList = [];
+  const repeatedItemsInMerge = [];
+  const finalList = [];
+
+  if (firstList.length > 1) {
+    const sortedFirstList = firstList.sort();
+    for (let i = 0; i < sortedFirstList.length - 1; i++) {
+      if (sortedFirstList[i + 1] === sortedFirstList[i]) {
+        if (sortedFirstList[i + 1] !== sortedFirstList[i + 2]) {
+          filteredFirstList.push(sortedFirstList[i + 1]);
+        }
+        repeatedItemsInFirsList.push(sortedFirstList[i]);
       } else {
-        itemsToConcat.push(secondList[k]);
+        filteredFirstList.push(sortedFirstList[i]);
       }
     }
-    const finalList = firstList.concat(itemsToConcat);
-    response.repeatedItems = repeatedItems;
-    response.filteredList = finalList;
-    return response;
+  } else {
+    filteredFirstList.push(firstList[0]);
   }
+  if (secondList.length > 1) {
+    const sortedSecondList = secondList.sort();
+    for (let i = 0; i < sortedSecondList.length - 1; i++) {
+      if (sortedSecondList[i + 1] === sortedSecondList[i]) {
+        if (sortedFirstList[i + 1] !== sortedFirstList[i + 2]) {
+          filteredSecondList.push(sortedFirstList[i + 1]);
+        }
+        repeatedItemsInSecondList.push(sortedSecondList[i]);
+      } else {
+        filteredSecondList.push(sortedSecondList[i]);
+      }
+    }
+  } else {
+    filteredSecondList.push(secondList[0]);
+  }
+  const concatenatedList = filteredFirstList.concat(filteredSecondList);
+  const sortedConcatenatedList = concatenatedList.sort();
+  if (sortedConcatenatedList.length === 2) {
+    if (sortedConcatenatedList[0] === sortedConcatenatedList[1]) {
+      finalList.push(sortedConcatenatedList[0]);
+    } else {
+      finalList.push(sortedConcatenatedList[0], sortedConcatenatedList[1]);
+    }
+  } else {
+    for (let i = 0; i < sortedConcatenatedList.length - 1; i++) {
+      if (sortedConcatenatedList[i + 1] === sortedConcatenatedList[i]) {
+        if (sortedConcatenatedList[i + 1] !== sortedConcatenatedList[i + 2]) {
+          finalList.push(sortedConcatenatedList[i + 1]);
+        }
+        repeatedItemsInMerge.push(sortedConcatenatedList[i]);
+      } else {
+        finalList.push(sortedConcatenatedList[i]);
+      }
+    }
+  }
+  response.repeatedItemsInFirsList = repeatedItemsInFirsList;
+  response.repeatedItemsInSecondList = repeatedItemsInSecondList;
+  response.repeatedItemsInMerge = repeatedItemsInMerge;
+  response.finalList = finalList;
+  return response;
 }
 
 export default Filter;
